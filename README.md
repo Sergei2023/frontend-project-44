9e60f1d13e75606fd2dd43a3c31f309363bdbd51
Sergei <serbogomya2022@gmail.com>
2023-09-25 07:59:40 +0300

    6

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/e4da2596f840df8c646f/maintainability)](https://codeclimate.com/github/Sergei2023/frontend-project-44/maintainability)
 
 https://asciinema.org/connect/67698752-69cc-4df7-a5f5-5462f5086ecd // запись начала игры, выйгрыша, проигрыша. Это шаг номер 5.
+
+ https://asciinema.org/connect/67698752-69cc-4df7-a5f5-5462f5086ecd // запись начала, выйгрыша, проигрыша. Это шаг номер 6
---
+++ src/games/calc-game.js
@@ -0,0 +1,30 @@
+import { getRandomNum } from "../cli.js";
+
+const calcRound = () => {
+  const firstOperand = getRandomNum(1, 20);
+  const secondOperand = getRandomNum(1, 20);
+  const operatorIndex = getRandomNum(0, 2);
+  const operators = ['+', '-', '*'];
+  const operator = operators[operatorIndex];
+  let correctAnswer = 0;
+
+  switch (operatorIndex) {
+    case 0:
+      correctAnswer = firstOperand + secondOperand;
+      break;
+    case 1:
+      correctAnswer = firstOperand - secondOperand;
+      break;
+    case 2:
+      correctAnswer = firstOperand * secondOperand;
+      break;
+    default:
+      correctAnswer = 1;
+  }
+
+  const question = `${firstOperand} ${operator} ${secondOperand}`;
+
+  return [question, correctAnswer.toString()];
+};
+
+export default calcRound;

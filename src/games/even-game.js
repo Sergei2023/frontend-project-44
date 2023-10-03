7ca3cf601684788d1ce85b4fabc0c908b5579dc1
Sergei <serbogomya2022@gmail.com>
2023-10-03 21:24:29 +0300

    fix

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,4 +1,4 @@
-import { getRandomNum } from '../cli.js';
+import { rand } from '../cli.js';
 import { phrases } from '../../phrases.js';
  
 
@@ -6,7 +6,7 @@ const isEven = (num) => num % 2 === 0;
 
 const evenRound = () => {
     
-  const question = getRandomNum(1, 10);
+  const question = rand(1, 10);
   const correctAnswer = isEven(question) ? phrases.YES : phrases.NO;
 
   return [question, correctAnswer];
